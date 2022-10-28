953224da7d45a113bb535440e3c570c05cc2975d
oym3778 <oym3778@MSS2150-10>
2022-10-28 18:28:32 -0400

    Everything Omra did

--- Decay/Assets/Scripts/Player.cs
+++ Decay/Assets/Scripts/Player.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    Vector3 position = Vector3.zero;
+    Vector3 direction = Vector3.zero;
+    Vector3 velocity = Vector3.zero;
+    [SerializeField] float speed = 1f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        position = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        velocity = direction * speed * Time.deltaTime;
+
+        transform.position = position;
     }
 }
