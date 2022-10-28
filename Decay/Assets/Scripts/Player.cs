7ced32968f6b7823280c71b1e5a0cb0ce2316c67
oym3778 <oym3778@MSS2150-10>
2022-10-28 19:37:05 -0400

    Bullet and Player script changed

--- Decay/Assets/Scripts/Player.cs
+++ Decay/Assets/Scripts/Player.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
     Vector3 position = Vector3.zero;
     Vector3 direction = Vector3.zero;
     Vector3 velocity = Vector3.zero;
+
     [SerializeField] float speed = 1f;
+
+    public Vector3 mousePos;
 
     // Start is called before the first frame update
     void Start()
@@ -18,8 +22,31 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        velocity = direction * speed * Time.deltaTime;
+        mousePos = Mouse.current.position.ReadValue();
+        mousePos = Camera.main.ScreenToWorldPoint(mousePos);
 
+        velocity = direction * speed * Time.deltaTime;
+        position += velocity;
         transform.position = position;
     }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        direction = context.ReadValue<Vector2>();
+        if (direction != Vector3.zero)
+        {
+            transform.rotation = Quaternion.LookRotation(Vector3.back, direction);
+        }
+        //}
+        //if (direction == Vector3.down)
+        //{
+        //    transform.rotation = Quaternion.LookRotation(Vector3.back, direction);
+        //}
+
+    }
+    //public void OnFire()
+    //{
+    //    Bullet cloneBullet = new Bullet();
+    //    Instantiate(cloneBullet, transform);
+    //}
 }
