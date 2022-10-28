3745e8ed6d09196adeb9f8b463b4d35ca1b1991a
Logan Holmes (RIT Student) <lsh8770@rit.edu>
2022-10-28 18:33:55 -0400

    fixing stupid idiotic git idiocy

---
+++ Decay/Assets/Scripts/ChunkSpawner.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChunkSpawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void SpawnCircular(int ammount, int diameter, Vector2 origin)
+    {
+        Transform transform = new Transform();
+        transform.position = origin;
+        for (int i = 0; i<ammount; i++){
+                        
+        }
+    }
+}
